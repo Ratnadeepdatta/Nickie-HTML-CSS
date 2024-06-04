965dc1e34af72f55e7d602744143bd6311574a0a
Ratnadeep Datta <162614054+Ratnadeepdatta@users.noreply.github.com>
2024-06-04 23:03:21 +0530

    Craete Homr Page

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const navBtn = document.querySelector(".btn-two");
+const navEl = document.querySelector(".nav");
+
+navBtn.addEventListener("click", function () {
+    navEl.classList.toggle("nav_open")
+})
